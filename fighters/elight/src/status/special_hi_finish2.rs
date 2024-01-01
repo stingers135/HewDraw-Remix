a61231e12e216f9158be48c8fefaabc9e5880e94
stingers135 <115596132+stingers135@users.noreply.github.com>
2023-12-31 19:31:01 -0500

    jobs done
    jobs done

--- fighters/elight/src/status/special_hi_finish2.rs
+++ fighters/elight/src/status/special_hi_finish2.rs
@@ -28,6 +28,7 @@ unsafe extern "C" fn special_hi_finish2_pre(fighter: &mut L2CFighterCommon) -> L
     );
     
     VarModule::on_flag(fighter.battle_object, vars::elight::instance::DISABLE_SPECIAL_HI);
+    VarModule::on_flag(fighter.battle_object, vars::elight::instance::DISABLE_SPECIAL_S);
     super::Set_Pyra_Up_Special_Cancel(fighter, true);
 
     0.into()
@@ -58,9 +59,16 @@ unsafe extern "C" fn special_hi_finish2_main(fighter: &mut L2CFighterCommon) -> 
 }
 
 unsafe extern "C" fn special_hi_finish2_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
-    // [h] when the motion is over disable special hi jump and special s
+    // [h] when the motion is over disable special hi jump and special s on aerial version
     if MotionModule::is_end(fighter.module_accessor) {
         VarModule::on_flag(fighter.battle_object, vars::elight::instance::DISABLE_SPECIAL_HI);
+
+        if fighter.is_flag(*FIGHTER_ELIGHT_STATUS_SPECIAL_HI_FLAG_GROUND_START) {
+            VarModule::off_flag(fighter.battle_object, vars::elight::instance::DISABLE_SPECIAL_S);
+        } else {
+            VarModule::on_flag(fighter.battle_object, vars::elight::instance::DISABLE_SPECIAL_S);
+        }
+
         if VarModule::is_flag(fighter.battle_object, vars::elight::instance::UP_SPECIAL_FREEFALL) {
             fighter.change_status(FIGHTER_STATUS_KIND_FALL_SPECIAL.into(), true.into());
         }
